baac9cb46a71879e9afe1b0327a7cb86f67992f9
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-16 13:32:48 +0000

    initial choices array added

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,37 +9,31 @@ startButton.addEventListener("click", function(event) {
 
 })
 
-var questionTitle = ["Which of the following keywords is used to define a variable in Javascript?", 
+var questionTitle = ["Javascrilpt is a _____-side programming language",
+"Which of the following keywords is not used to define a variable in Javascript?", 
+"JavaScript is the same as Java",
 "Which of the following methods can be used to display data in some form using Javascript?",
-"What types of data are used in Javascript?",
-"Which of the below is a type of object used in Javascript?",
-"Which of the below are ways to state objects in Javasctipt?",
+"Which of the below are datatypes used in Javascript?",
 "Which of the following is not a Javascript framework?",
 "How do we write a comment in javascript?",
-"Which built-in method returns the calling string value converted to lower case?",
-"Which of the following function of String object combines the text of two strings and returns a new string?",
+"What ",
 "Upon encountering empty statements, what does the Javascript Interpreter do?",
+"Which of the following function of String object combines the text of two strings and returns a new string?",
 ]
 
 console.log(questionTitle);
 
-// types of data:
-// string
-// number
-// boolean
-// object
-// function
+var choices = [
+    ["Server", "Client", "both", "Neither"],
+    ["var", "const", "let", "obj"],
+    ["True", "False"],
+    ["console.log()", "document.wrtie()", "window.write()", "page.log()"],
+    ["string, number, boolean, object", "number, function, object, maps", "boolean, object, array, input", "object, number, time, boolean"],
+    ["React", "Vue", "Cassandra", "Node"],
+    ["//", "%", "#", "--"],
+    ["Nan", "B", "C", "D"],
+    ["E", "F", "G", "H"],
+    ["I", "J", "K", "L"]
+]
 
-// types of objects:
-
-// Object
-// Date
-// Array
-// String
-// Number
-// Boolean
-
-// Node
-// Vue
-// React
-// Cassandra
+console.log(choices)
