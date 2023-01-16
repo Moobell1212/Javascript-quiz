c510bb349d8d9d9d6a4c16b42c54b9cc18798963
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-01-16 20:01:26 +0000

    answers object expanded

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,45 +3,66 @@ var startScreen = document.querySelector("#start-screen");
 var questions = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var answerChoices = document.querySelector("#choices");
+var endScreen = document.querySelector("#end-screen");
+var finalScore = document.querySelector("#final-score");
+var submitInitials = document.querySelector("#submit");
 
 // function added to start the quiz when the start button is clicked.
-startButton.addEventListener("click", function (event) {
+startButton.addEventListener("click", function () {
     startScreen.style.display = "none";
     console.log(questions);
     questions.classList.remove("hide");
     // questions.style.backgroundColor = "red";
     runQuiz();
-})
 
-var Q = 0;
-
-function createQuestion() {
-    answerChoices.innerHTML = "";
-    questionTitle.textContent = questionsArr[Q].text
-    questionsArr[Q].choices.forEach(function(choice){
-        var button = document.createElement("button");
-        button.textContent = choice;
-        button.onclick = function () {
-            console.log(choice);
-            if (choice === questionsArr[Q].correctAnswer){
-                console.log("correct");
-            }
-            else {
-                console.log("Incorrect");
-            }
-            Q++;
-            if (Q === questionsArr.length){
-                console.log("endgame")
-            }
-            else {
-            createQuestion();
-            }
-        }
-        answerChoices.appendChild(button);
-    })
-}
+});
 
 function runQuiz() {
     console.log(questions);
     createQuestion()
+};
+
+
+var Q = 0;
+var score = 0;
+
+function createQuestion() {
+    answerChoices.innerHTML = "";
+    questionTitle.textContent = questionsArr[Q].text
+    questionsArr[Q].choices.forEach(function (choice) {
+        var button = document.createElement("button");
+        button.textContent = choice;
+        button.onclick = function () {
+            console.log(choice);
+            if (choice === questionsArr[Q].correctAnswer) {
+                console.log("Correct");
+                score++;
+            }
+            else {
+                console.log("Incorrect");
+                score--;
+            }
+            Q++;
+            if (Q === questionsArr.length) {
+                console.log("endgame")
+                allDone()
+            }
+            else {
+                createQuestion();
+            }
+        }
+        answerChoices.appendChild(button);
+    })
+};
+
+function allDone() {
+    questions.classList.add("hide");
+    endScreen.classList.remove("hide");
+    finalScore.textContent = score;
+};
+
+submitInitials.addEventListener("click", function () {
+    var userInitials = document.querySelector("#initials").value;
+    localStorage.setItem("UserInitials", userInitials);
 }
+);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,3 @@
-var startButton = document.querySelector("#start");
-var startScreen = document.querySelector("#start-screen");
-var questions = document.querySelector("#questions");
-var questionTitle = document.querySelector("#question-title");
-var answerChoices = document.querySelector("#choices");
-
 var questionsArr = [
     {
         text: "Javascrilpt is a _____-side programming language",
@@ -14,33 +8,38 @@ var questionsArr = [
         text:"Which of the following keywords is not used to define a variable in Javascript?",
         choices: ["var", "const", "let", "obj"],
         correctAnswer: "obj"
+    },
+    {
+        text:"JavaScript is the same as Java.",
+        choices: ["True", "False"],
+        correctAnswer: "False"
+    },
+    {
+        text:"Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["document.write()", "console.log()", "window.write()", "page.log()"],
+        correctAnswer: "console.log()"
+    },
+    {
+        text:"Which of the below are datatypes used in Javascript?",
+        choices: ["string, number, boolean, object", "number, function, object, maps", "boolean, object, array, input", "object, number, time, boolean"],
+        correctAnswer: "string, number, boolean, object"
+    },
+    {
+        text:"Which of the following is not a Javascript framework?",
+        choices:["React", "Vue", "Cassandra", "Node"],
+        correctAnswer: "Cassandra"
+    },
+    {
+        text:"How does one write a comment in javascript?",
+        choices:["//", "%", "#", "--"],
+        correctAnswer: "//"
+    },
+    {
+        text:"Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["log an error", "nothing", "stop running"],
+        correctAnswer: "nothing"
     }
-
 ]
 
-// var questionTitle = [,
-//     "JavaScript is the same as Java",
-//     "Which of the following methods can be used to display data in some form using Javascript?",
-//     "Which of the below are datatypes used in Javascript?",
-//     "Which of the following is not a Javascript framework?",
-//     "How do we write a comment in javascript?",
-//     "What ",
-//     "Upon encountering empty statements, what does the Javascript Interpreter do?",
-//     "Which of the following function of String object combines the text of two strings and returns a new string?",
-// ]
 
-
-// var choices = [
-    
-//     ["True", "False"],
-//     ["console.log()", "document.wrtie()", "window.write()", "page.log()"],
-//     ["string, number, boolean, object", "number, function, object, maps", "boolean, object, array, input", "object, number, time, boolean"],
-//     ["React", "Vue", "Cassandra", "Node"],
-//     ["//", "%", "#", "--"],
-//     ["Nan", "B", "C", "D"],
-//     ["E", "F", "G", "H"],
-//     ["I", "J", "K", "L"]
-// ];
-
-// var correctAnswers = [] ?????
 
